c2e4682ddbaad553f677688aa65241a173592d33
fauziilyassaputra <fauziilyassaputra@gmail.com>
2026-04-20 17:26:50 +0700

    firts commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,5 +14,13 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
+eslintConfig.push({
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "react/jsx-key": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+  },
+});
 
 export default eslintConfig;
