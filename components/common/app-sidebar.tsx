e914836351391006a66f8e6ba9dc73eeb243165f
fauziilyassaputra <fauziilyassaputra@gmail.com>
2026-04-24 13:46:30 +0700

    create sidebar in admin page

---
+++ components/common/app-sidebar.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { EllipsisVertical, LogOut, WashingMachine } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "../ui/sidebar";
+
+export default function AppSidebar() {
+  const { isMobile } = useSidebar();
+  return (
+    <Sidebar>
+      {/*  */}
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <div className="flex items-center gap-2 self-center font-medium">
+                <div className="bg-teal-500 flex p-2 items-center justify-center rounded-md">
+                  <WashingMachine className="size-4" />
+                </div>
+                POS Laundry
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton size="lg">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src="" />
+                    <AvatarFallback className="rounded-lg">A</AvatarFallback>
+                  </Avatar>
+                  <div className="leading-tight">
+                    <h4 className="truncate font-medium">Wise</h4>
+                    <p className="text-muted-foreground truncate text-xs">
+                      Manager
+                    </p>
+                  </div>
+                  <EllipsisVertical className="ml-auto size-4" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="min-w-56 rounded-lg"
+                side={isMobile ? "bottom" : "right"}
+                align="end"
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className="p-0 font-normal">
+                  <div className="flex items-center gap-2 px-1 py-1.5">
+                    <Avatar className="h-8 w-8 rounded-lg">
+                      <AvatarImage src="" />
+                      <AvatarFallback className="rounded-lg">A</AvatarFallback>
+                    </Avatar>
+                    <div className="leading-tight">
+                      <h4 className="truncate font-medium">Wise</h4>
+                      <p className="text-muted-foreground truncate text-xs">
+                        Manager
+                      </p>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <LogOut />
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
